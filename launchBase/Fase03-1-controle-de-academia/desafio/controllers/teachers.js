b255e65b535fa8fb151e831e15a3013cce9fe7aa
Lucas Ferreira <lucasferreira@nepomuceno.exp>
2020-03-03 07:40:20 -0300

    Desafio 4.1 e 4.2

---
+++ launchBase/Fase03-1-controle-de-academia/desafio/controllers/teachers.js
@@ -0,0 +1,8 @@
+
+exports.index = (req,res) => {
+  return res.render("teachers/index")
+}
+
+exports.show = (req, res) => {
+  return res.render("teachers/show")
+}
--- launchBase/Fase03-1-controle-de-academia/desafio/routes.js
+++ launchBase/Fase03-1-controle-de-academia/desafio/routes.js
@@ -1,30 +1,15 @@
 const express = require('express')
 const routes = express.Router()
-const instructors = require('./controllers/instructors')
-const members = require('./controllers/members')
+const teachers = require('./controllers/teachers')
+const students = require('./controllers/students')
 
 routes.get('/', (req,res) => {
-  return res.redirect('/instructors')
+  return res.redirect('/teachers')
 })
 
-// ######## INSTRUCTORS ########
+// ### TEACHERS ###
 
-routes.get('/instructors', instructors.index)
-routes.get('/instructors/create', instructors.create)
-routes.get('/instructors/:id', instructors.show)
-routes.get('/instructors/:id/edit', instructors.update)
-routes.post('/instructors', instructors.post)
-routes.put('/instructors', instructors.put)
-routes.delete('/instructors', instructors.delete)
-
-// ######## MEMBERS ########
-
-routes.get('/members', members.index)
-routes.get('/members/create', members.create)
-routes.get('/members/:id', members.show)
-routes.get('/members/:id/edit', members.update)
-routes.post('/members', members.post)
-routes.put('/members', members.put)
-routes.delete('/members', members.delete)
+routes.get('/teachers',teachers.index)
+routes.get('/teachers/:id',teachers.show)
 
 module.exports = routes
